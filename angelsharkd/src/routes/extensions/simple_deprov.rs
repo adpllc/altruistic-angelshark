5e82b9ed1f7f236d596500478588dd2f84231fef
Carpenter, Adam (CORP) <adam.carpenter@adp.com>
2021-12-08 15:29:59 -0500

    feat: begin basic filter

--- angelsharkd/src/routes/extensions/simple_deprov.rs
+++ angelsharkd/src/routes/extensions/simple_deprov.rs
@@ -1,5 +1,12 @@
-use warp::{Filter, Rejection, Reply};
+use warp::{
+    body::{content_length_limit, json},
+    post, Filter, Rejection, Reply,
+};
 
 pub fn filter() -> impl Filter<Extract = impl Reply, Error = Rejection> + Clone {
-    warp::path("deprov").map(|| -> &str { todo!() }) // TODO:
+    warp::path("deprov")
+        .and(post())
+        .and(content_length_limit(1024 * 16))
+        .and(json())
+        .map(|_: String| -> &str { todo!() }) // TODO:
 }
