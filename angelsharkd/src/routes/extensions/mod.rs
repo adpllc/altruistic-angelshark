e65a8bf77efa9c7b28d960f9ac6cffbff5d31dd4
Carpenter, Adam (CORP) <adam.carpenter@adp.com>
2022-01-31 15:37:25 -0500

    fix: remove warning about unused config with no extensions enabled

--- angelsharkd/src/routes/extensions/mod.rs
+++ angelsharkd/src/routes/extensions/mod.rs
@@ -10,7 +10,7 @@ mod simple_search;
 
 /// The extension filter; consists of all compiled optional Angelshark extension
 /// filters combined under `/extensions`.
-pub fn filter(config: &Config) -> impl Filter<Extract = impl Reply, Error = Rejection> + Clone {
+pub fn filter(_config: &Config) -> impl Filter<Extract = impl Reply, Error = Rejection> + Clone {
     // Note: this next line deals with the common edge case of having no
     // extensions loaded with feature flags. It ensures that the the type
     // checking is right when the return `.and()` is called below.
@@ -19,20 +19,20 @@ pub fn filter(config: &Config) -> impl Filter<Extract = impl Reply, Error = Reje
     // Block to enable simple_search extension feature. Instantiates a
     // searchable haystack and configures filters to handle search requests.
     #[cfg(feature = "simple_search")]
-    let haystack = simple_search::Haystack::new(config.runner.clone());
+    let haystack = simple_search::Haystack::new(_config.runner.clone());
     #[cfg(feature = "simple_search")]
     let filters = filters
         .or(simple_search::search_filter(haystack.clone()))
         .or(simple_search::refresh_filter(haystack));
 
     #[cfg(feature = "simple_deprov")]
-    let filters = filters.or(simple_deprov::filter(config.runner.clone()));
+    let filters = filters.or(simple_deprov::filter(_config.runner.clone()));
 
     #[cfg(feature = "simple_busy")]
     let filters = filters
-        .or(simple_busy::busy_filter(config.runner.to_owned()))
-        .or(simple_busy::release_filter(config.runner.to_owned()))
-        .or(simple_busy::toggle_filter(config.runner.to_owned()));
+        .or(simple_busy::busy_filter(_config.runner.to_owned()))
+        .or(simple_busy::release_filter(_config.runner.to_owned()))
+        .or(simple_busy::toggle_filter(_config.runner.to_owned()));
 
     path("extensions").and(filters)
 }
